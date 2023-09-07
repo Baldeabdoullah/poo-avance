9228adb4a2ce0da5f259af7fd957cdb144dca46b
Aboullah Balde <abdoullahbalde@gmail.com>
2023-09-07 16:36:34 +0200

    poo avance

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+
+require_once('./libraries/controllers/Article.php');
+
+$controller = new \Controllers\Article();
+
+$controller->index();
---
+++ libraries/database.php
@@ -0,0 +1,12 @@
+<?php
+
+// RETOURNE UNE CONNEXION A LA BASE DE DONNE
+function getPdo(): PDO
+{
+    $pdo = new PDO('mysql:host=localhost;dbname=blogpoo;charset=utf8', 'root', '', [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+    ]);
+
+    return $pdo;
+}
